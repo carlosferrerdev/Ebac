8e41639b4baf51e93f23d1f10f48200b5fdac652
Carlos Ferrer <carlosvferrer@gmail.com>
2023-03-23 18:21:54 -0300

    Initial commit

---
+++ src/Exercicios/calculaMedia.java
@@ -0,0 +1,12 @@
+package Exercicios;
+
+public class calculaMedia {
+    public static void main(String[] args) {
+        double nota1 = 10d;
+        double nota2 = 8.5d;
+        double nota3 = 9d;
+        double nota4 = 6.9d;
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A média das notas é: " + media);
+    }
+}
